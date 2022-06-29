f5c16fec3c7f12d19764c8589bb77fbbca66418b
Leandro Viana <leandroviana.vs@gmail.com>
2022-06-29 11:35:55 -0300

    add hover on icons

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -1,18 +1,36 @@
-import { Trash, Circle, CheckCircle } from 'phosphor-react';
+import { Trash, Circle, CheckCircle, IconWeight } from 'phosphor-react';
+import { useState } from 'react';
 import styles from './Tasks.module.css';
 
 export function Tasks(){
 
-    
+    const [CircleWeight, setCircleWeight] = useState<IconWeight>("bold");
+    const [CheckCicleColor, setCheckCicleColor] = useState<IconWeight>("var(--purple-dark)");
+
+
     return (
         <ul className={styles.tasklist}>            
             <li className={styles.taskitem}>
-                <Circle weight='bold' cursor="pointer" size={32} className={styles.circle} />
+                <Circle 
+                color="var(--blue)"
+                 weight={CircleWeight}
+                 cursor="pointer" 
+                 size={32} 
+                 className={styles.circle} 
+                 onMouseOver={() => setCircleWeight('duotone')}
+                 onMouseLeave={() => setCircleWeight('bold')}/>
+                
                 <span>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
                 <Trash cursor="pointer" size={32} className={styles.trash} />
             </li>
             <li className={styles.taskitemCompleted}>
-                <CheckCircle color="var(--purple-dark)" cursor='pointer' weight='fill' size={24} />
+                <CheckCircle 
+                color={CheckCicleColor} 
+                cursor='pointer' 
+                weight='fill'
+                size={24} 
+                onMouseOver={() => setCheckCicleColor('var(--purple)')}
+                onMouseLeave={() => setCheckCicleColor('var(--purple-dark)')}/>
                     <span>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
                 <Trash cursor="pointer" size={32} className={styles.trash} />
             </li> 
